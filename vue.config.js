a5215a80d44feeaefbaf556288820ad86b4fbee5
Drunken1sailor <alexeevilya.ykt2014@mail.ru>
2024-01-18 19:13:38 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base:'/vue-example/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-example/'
+    : '/',
   transpileDependencies: true
 })
